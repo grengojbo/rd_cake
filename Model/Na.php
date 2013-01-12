8484a52cd4b3fd88e592a397ae20a966c41b825a
dvdwalt <dvdwalt@e9fd4974-e3b9-4200-b821-f6b099dd8586>
2013-01-12 14:52:14 +0000

    CSV export function prototype 2
    git-svn-id: http://svn.code.sf.net/p/radiusdesk/code/trunk/rd_cake@58 e9fd4974-e3b9-4200-b821-f6b099dd8586

--- Model/Na.php
+++ Model/Na.php
@@ -7,7 +7,7 @@ App::uses('AppModel', 'Model');
  */
 class Na extends AppModel {
 
-    public $actsAs = array('Containable');
+    public $actsAs = array('Containable','CsvExport');
 
 	public $displayField = 'nasname';
 
