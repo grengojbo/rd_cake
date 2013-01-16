e3509296073d63a776e4fa687dd4e4ece1b0ac36
dvdwalt <dvdwalt@e9fd4974-e3b9-4200-b821-f6b099dd8586>
2013-01-16 13:21:53 +0000

    Redo Access Providers/3
    git-svn-id: http://svn.code.sf.net/p/radiusdesk/code/trunk/rd_cake@72 e9fd4974-e3b9-4200-b821-f6b099dd8586

--- Config/RadiusDesk.php
+++ Config/RadiusDesk.php
@@ -9,10 +9,10 @@ $config['language']['default']    = '4_4';     //This is the id 4 of Languages a
 $config['commands']['msgcat'] = '/usr/bin/msgcat';
 
 //Define the connection types and if they are active or not
-$config['conn_type'][0]     = array('name' => 'Direct (Fixed IP)',  'id' => 'direct',   'active' => true);
-$config['conn_type'][1]     = array('name' => 'OpenVPN',            'id' => 'openvpn',  'active' => true);
-$config['conn_type'][2]     = array('name' => 'PPTP',               'id' => 'pptp',     'active' => false);
-$config['conn_type'][3]     = array('name' => 'Dynamic Client',     'id' => 'dynamic',  'active' => true);
+$config['conn_type'][0]     = array('name' => __('Direct (Fixed IP)'),  'id' => 'direct',   'active' => true);
+$config['conn_type'][1]     = array('name' => __('OpenVPN'),            'id' => 'openvpn',  'active' => true);
+$config['conn_type'][2]     = array('name' => __('PPTP'),               'id' => 'pptp',     'active' => false);
+$config['conn_type'][3]     = array('name' => __('Dynamic Client'),     'id' => 'dynamic',  'active' => true);
 
 //Define the location of ccd (client config directory)
 //FIXME This value does not get read by the OpenvpnClients Model - investigate
