3f8065f3dd0bddbd9a4bf918b2d1aa0a8f0ae450
dvdwalt <dvdwalt@e9fd4974-e3b9-4200-b821-f6b099dd8586>
2013-05-21 11:03:18 +0000

    Dynamic Logina pages...
    git-svn-id: http://svn.code.sf.net/p/radiusdesk/code/trunk/rd_cake@321 e9fd4974-e3b9-4200-b821-f6b099dd8586

---
+++ Model/DynamicPair.php
@@ -0,0 +1,41 @@
+<?php
+App::uses('AppModel', 'Model');
+/**
+ * Realm Model
+ *
+ * @property User $User
+ */
+class DynamicPair extends AppModel {
+
+    public $actsAs = array('Containable');
+
+
+/**
+ * Display field
+ *
+ * @var string
+ */
+	public $displayField = 'name';
+
+/**
+ * Validation rules
+ *
+ * @var array
+ */
+	public $validate = array(
+        'name' => array(
+            'required' => array(
+                'rule' => array('notEmpty'),
+                'message' => 'Value is required'
+            )
+        )
+    );
+	//The Associations below have been created with all possible keys, those that are not needed can be removed
+
+    public $belongsTo = array(
+        'DynamicDetail' => array(
+            'className'     => 'DynamicDetail',
+			'foreignKey'    => 'dynamic_detail_id'
+        )
+	);
+}
