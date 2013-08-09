f70f5e92e96f387e2764aed33b6beae141b1ef85
dvdwalt <dvdwalt@e9fd4974-e3b9-4200-b821-f6b099dd8586>
2013-08-09 18:24:32 +0000

    pdf final 3
    git-svn-id: http://svn.code.sf.net/p/radiusdesk/code/trunk/rd_cake@467 e9fd4974-e3b9-4200-b821-f6b099dd8586

--- Config/RadiusDesk.php
+++ Config/RadiusDesk.php
@@ -49,6 +49,14 @@ $config['voucher_formats'][0]     = array('name' => 'Generic A4',               
 $config['voucher_formats'][1]     = array('name' => 'Generic A4 Page/Voucher',  'id' => 'a4_page',          'active' => true);
 $config['voucher_formats'][2]     = array('name' => 'Avery 5160',               'id' => '5160',             'active' => true);
 $config['voucher_formats'][3]     = array('name' => 'Avery 5161',               'id' => '5161',             'active' => true);
+$config['voucher_formats'][4]     = array('name' => 'Avery 5162',               'id' => '5162',             'active' => true);
+$config['voucher_formats'][5]     = array('name' => 'Avery 5163',               'id' => '5163',             'active' => true);
+$config['voucher_formats'][6]     = array('name' => 'Avery 5164',               'id' => '5164',             'active' => false); //gives trouble
+$config['voucher_formats'][7]     = array('name' => 'Avery 8600',               'id' => '8600',             'active' => true); 
+$config['voucher_formats'][8]     = array('name' => 'Avery L7160',              'id' => 'L7160',            'active' => true); 
+$config['voucher_formats'][9]     = array('name' => 'Avery L7161',              'id' => 'L7161',            'active' => true); 
+$config['voucher_formats'][10]     = array('name' => 'Avery L7163',              'id' => 'L7163',            'active' => true); 
+
 
 
 //FIXME To incorporate
@@ -127,7 +135,5 @@ $config['experimental']['eduroam']['radius_secret'] = 'eduroam';
 $config['experimental']['snmp']['ro']               = 'public';
 $config['experimental']['snmp']['rw']               = 'private';
 $config['experimental']['snmp']['contact']          = 'radiusdesk@gmail.com';
-
-
 
 ?>
